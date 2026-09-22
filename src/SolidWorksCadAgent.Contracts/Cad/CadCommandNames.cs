b37f912f1d4429ea7938bb88c52716bfa25ff34d
jhaago <jordanhaagensen@gmail.com>
2026-09-22 10:24:13 +0800

    feat: add initial CAD command names

---
+++ src/SolidWorksCadAgent.Contracts/Cad/CadCommandNames.cs
@@ -0,0 +1,21 @@
+namespace SolidWorksCadAgent.Contracts.Cad
+{
+    public static class CadCommandNames
+    {
+        public const string AttachSolidWorks = "AttachSolidWorks";
+        public const string LaunchSolidWorks = "LaunchSolidWorks";
+        public const string NewPart = "NewPart";
+        public const string OpenPart = "OpenPart";
+        public const string SavePart = "SavePart";
+        public const string CreateSketch = "CreateSketch";
+        public const string AddRectangle = "AddRectangle";
+        public const string AddCircle = "AddCircle";
+        public const string ExitSketch = "ExitSketch";
+        public const string Extrude = "Extrude";
+        public const string CutExtrude = "CutExtrude";
+        public const string Rebuild = "Rebuild";
+        public const string GetBoundingBox = "GetBoundingBox";
+        public const string GetBodyCount = "GetBodyCount";
+        public const string GetRebuildErrors = "GetRebuildErrors";
+    }
+}
